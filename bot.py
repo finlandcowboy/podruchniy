581b21dc9b9503b8b5fa9b9772d0c9caaee5710e
Stepan <finlandcowboy@gmail.com>
2021-09-27 15:58:12 +0300

    забыл что пользоваться будет Витя

--- bot.py
+++ bot.py
@@ -1,7 +1,4 @@
 import telebot
-import schedule
-from threading import Thread
-import time
 bot = telebot.TeleBot('1088789624:AAH7fPuPwkFIdst-uu07Nl4X2XSYXWFX6D4')
 
 @bot.message_handler(commands=['start'])
@@ -10,9 +7,12 @@ def mes(message):
     bot.register_next_step_handler(msg, div)
 
 def div(message):
-    import podseti
-    divide = podseti.subnet_divide(int(message.text.split()[0]), int(message.text.split()[1]))
-    bot.send_message(message.chat.id, "\n".join(divide))
+    try:
+        import podseti
+        divide = podseti.subnet_divide(int(message.text.split()[0]), int(message.text.split()[1]))
+        bot.send_message(message.chat.id, "\n".join(divide))
+    except:
+        bot.send_message(message.chat.id, "Ты еблан??? Написано же маску сука число блять число после слэша")
 
 
 bot.polling(True)
