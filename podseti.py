89c70b095135a62c6289a14df4e020d61c19adb9
Stepan <finlandcowboy@gmail.com>
2021-09-27 16:06:05 +0300

    fixed algorithm

--- podseti.py
+++ podseti.py
@@ -4,7 +4,7 @@ def subnet_divide(mask, subnets):
     addresses = 2 ** (32 - mask)
     output = []
     for i in range(subnets):
-        output.append(f'Подсеть {i}: 192.168.{int(i*(addresses/subnets)) + 1// 256}.{int(i*(addresses/subnets)) + 1} % 256')
+        output.append(f'Подсеть {i}: 192.168.{int(i*(addresses/subnets)) + 1// 256}.{int(i*(addresses/subnets)) + 1% 256 }  ')
     return output
 
 
